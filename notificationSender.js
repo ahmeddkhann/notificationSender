8d99de4e0a4c69af41f41bf4f435b9ad75891a4b
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-01 17:03:20 +0500

    some more logics are added

--- notificationSender.js
+++ notificationSender.js
@@ -12,4 +12,44 @@ const usePushNotifications = () => {
             shouldSetBadge: false
         })
     })
+
+    const [expoPushToken, setExpoPushToken] = useState();
+    const [notifications, setNotifications] = useState();
+    const notificationListner = useRef();
+    const notificationResponse = useRef();
+
+    async function registerForPushNotificationsAsync (){
+        let token ;
+
+        if (Device.isDevice){
+            const {status} = await Notifications.getPermissionsAsync()
+            let finalStatus = existingStatus;
+
+            if (existingStatus !== "granted"){
+                const {status} = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            };
+            if (existingStatus !== "granted"){
+                alert("Failed to get push token")
+            }
+
+            token = await Notifications.getExpoPushTokenAsync({
+                projectId: Constants.expoConfig?.extra?.eas?.projectId
+            });
+
+            if (Platform.OS == "android"){
+                Notifications.setNotificationChannelAsync("default", {
+                    name: "default",
+                    imposrtance: Notifications.AndroidImportance.MAX,
+                    vibrationPattern: [0, 250, 250, 250],
+                    lightColor: "red"
+                })
+            }
+
+            return token;
+        }else {
+            console.log("Please use a physical device");
+            
+        }
+    }
 }
